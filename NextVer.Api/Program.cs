00622598a657520da5b755fd671789093bc92ff2
Błażej Zajac <blazezaj@gmail.com>
2023-06-01 22:57:51 +0200

    Update Program.cs

--- NextVer.Api/Program.cs
+++ NextVer.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using NextVer.Infrastructure.Persistance;
+using NextVer.Infrastructure.Persistence.DbSeed;
 using NextVerBackend.Configuration;
 using System.Text;
 
@@ -12,8 +13,6 @@ var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<NextVerDbContext>(options => options
     .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
 
 builder.Services.AddControllers();
 builder.Services.ConfigureSwagger();
@@ -33,6 +32,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
@@ -42,6 +42,15 @@ if (!app.Environment.IsDevelopment())
 else
 {
     app.UseDeveloperExceptionPage();
+}
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<NextVerDbContext>();
+    var databaseSeed = new Seed(context);
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+    databaseSeed.SeedData();
 }
 
 app.UseSwagger();
