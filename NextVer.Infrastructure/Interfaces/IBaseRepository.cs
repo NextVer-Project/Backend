09455c96fdb5379befcb04899d6f803f027f733f
Błażej Zajac <blazezaj@gmail.com>
2023-09-06 17:52:48 +0200

    Create the IBaseRepository.cs interface

---
+++ NextVer.Infrastructure/Interfaces/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+
+namespace NextVer.Infrastructure.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<bool> Add(T entity);
+        Task<bool> Delete(int id);
+        Task<bool> Edit<TKey, TDto>(TKey id, TDto dto);
+        IQueryable<TLinkEntity> GetLinkEntitiesFor<TEntity, TLinkEntity>(
+             Expression<Func<TLinkEntity, bool>> filterExpression
+         )
+             where TEntity : class
+             where TLinkEntity : class, new();
+        void AddLinkEntity<TLinkEntity>(TLinkEntity entity) where TLinkEntity : class;
+        void RemoveLinkEntity<TLinkEntity>(TLinkEntity entity) where TLinkEntity : class;
+        Task<T> GetById(int id, params Expression<Func<T, object>>[] includes);
+        Task<bool> SaveChangesAsync();
+        Task<int> GetNumberOfEntities<T>() where T : class;
+        Task<IEnumerable<TEntity>> GetEntitiesBy<TEntity>(Expression<Func<TEntity, bool>> filterExpression)
+            where TEntity : class;
+    }
+}
