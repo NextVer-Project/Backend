04a53def05e419f00a54b4a9e741cecd28ba018b
Błażej Zajac <blazezaj@gmail.com>
2023-09-06 18:10:20 +0200

    Update AutoMapperProfiles.cs

--- NextVer.Api/Helpers/AutoMapperProfiles.cs
+++ NextVer.Api/Helpers/AutoMapperProfiles.cs
@@ -10,6 +10,51 @@ namespace NextVerBackend.Helpers
         {
             CreateMap<UserForRegisterDto, User>();
             CreateMap<UserForLoginDto, User>();
+            CreateMap<EmailForChange, User>();
+            CreateMap<MovieForAddDto, Movie>();
+            CreateMap<MovieForEditDto, Movie>();
+            CreateMap<MovieCountDto, Movie>();
+            CreateMap<TvShowForAddDto, TvShow>();
+            CreateMap<TvShowForEditDto, TvShow>();
+            CreateMap<TvShowCountDto, TvShow>();
+            CreateMap<EpisodeForAddDto, Episode>();
+            CreateMap<EpisodeForEditDto, Episode>();
+            CreateMap<ReleasePlaceTypeForAddDto, ReleasePlaceType>();
+            CreateMap<ReleasePlaceTypeForEditDto, ReleasePlaceType>();
+            CreateMap<ReleasePlaceForAddDto, ReleasePlace>();
+            CreateMap<ReleasePlaceForEditDto, ReleasePlace>();
+            CreateMap<GenreForAddDto, Genre>();
+            CreateMap<GenreForEditDto, Genre>();
+            CreateMap<UniverseForAddDto, Universe>();
+            CreateMap<UniverseForEditDto, Universe>();
+            CreateMap<MediaGalleryTypeForAddDto, MediaGalleryType>();
+            CreateMap<MediaGalleryTypeForEditDto, MediaGalleryType>();
+            CreateMap<GalleryForAddDto, Gallery>();
+            CreateMap<GalleryForEditDto, Gallery>();
+            CreateMap<NotificationTypeForAddDto, NotificationType>();
+            CreateMap<NotificationTypeForEditDto, NotificationType>();
+            CreateMap<NotificationForAddDto, Notification>();
+            CreateMap<NotificationForEditDto, Notification>();
+            CreateMap<RatingCategoryForAddDto, RatingCategory>();
+            CreateMap<RatingCategoryForEditDto, RatingCategory>();
+            CreateMap<RatingForAddDto, Rating>();
+            CreateMap<RatingForEditDto, Rating>();
+            CreateMap<TechnologyTypeForAddDto, TechnologyType>();
+            CreateMap<TechnologyTypeForEditDto, TechnologyType>();
+            CreateMap<TechnologyForAddDto, Technology>();
+            CreateMap<TechnologyForEditDto, Technology>();
+            CreateMap<UserTypeForAddDto, UserType>();
+            CreateMap<UserTypeForEditDto, UserType>();
+            CreateMap<UserCollectionTypeForAddDto, UserCollectionType>();
+            CreateMap<UserCollectionTypeForEditDto, UserCollectionType>();
+            CreateMap<UserCollectionForAddDto, UserCollection>();
+            CreateMap<UserCollectionForEditDto, UserCollection>();
+            CreateMap<ProductionTypeForAddDto, ProductionType>();
+            CreateMap<ProductionTypeForEditDto, ProductionType>();
+            CreateMap<ProductionVersionForAddDto, ProductionVersion>();
+            CreateMap<ProductionVersionForEditDto, ProductionVersion>();
+            CreateMap<CommentForAddDto, Comment>();
+            CreateMap<CommentForEditDto, Comment>();
         }
     }
 }
