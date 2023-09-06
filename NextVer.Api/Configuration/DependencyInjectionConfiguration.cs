fb3ba397483b166c74b24f8c4bde1d05a0d1bc1d
Błażej Zajac <blazezaj@gmail.com>
2023-09-06 18:09:36 +0200

    Update DependencyInjectionConfiguration.cs

--- NextVer.Api/Configuration/DependencyInjectionConfiguration.cs
+++ NextVer.Api/Configuration/DependencyInjectionConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using MailKit.Net.Smtp;
+using NextVer.Domain.Models;
 using NextVer.Infrastructure.Interfaces;
 using NextVer.Infrastructure.Repositories;
 using NextVer.Infrastructure.Services;
@@ -10,6 +11,29 @@ namespace NextVerBackend.Configuration
         public static void ConfigureDependencyInjection(this IServiceCollection services)
         {
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<ITvShowRepository, TvShowRepository>();
+            services.AddScoped<IBaseRepository<Movie>, BaseRepository<Movie>>();
+            services.AddScoped<IBaseRepository<TvShow>, BaseRepository<TvShow>>();
+            services.AddScoped<IBaseRepository<Episode>, BaseRepository<Episode>>();
+            services.AddScoped<IBaseRepository<Genre>, BaseRepository<Genre>>();
+            services.AddScoped<IBaseRepository<Universe>, BaseRepository<Universe>>();
+            services.AddScoped<IBaseRepository<Comment>, BaseRepository<Comment>>();
+            services.AddScoped<IBaseRepository<Gallery>, BaseRepository<Gallery>>();
+            services.AddScoped<IBaseRepository<MediaGalleryType>, BaseRepository<MediaGalleryType>>();
+            services.AddScoped<IBaseRepository<Notification>, BaseRepository<Notification>>();
+            services.AddScoped<IBaseRepository<NotificationType>, BaseRepository<NotificationType>>();
+            services.AddScoped<IBaseRepository<ProductionType>, BaseRepository<ProductionType>>();
+            services.AddScoped<IBaseRepository<ProductionVersion>, BaseRepository<ProductionVersion>>();
+            services.AddScoped<IBaseRepository<RatingCategory>, BaseRepository<RatingCategory>>();
+            services.AddScoped<IBaseRepository<Rating>, BaseRepository<Rating>>();
+            services.AddScoped<IBaseRepository<ReleasePlaceType>, BaseRepository<ReleasePlaceType>>();
+            services.AddScoped<IBaseRepository<ReleasePlace>, BaseRepository<ReleasePlace>>();
+            services.AddScoped<IBaseRepository<TechnologyType>, BaseRepository<TechnologyType>>();
+            services.AddScoped<IBaseRepository<Technology>, BaseRepository<Technology>>();
+            services.AddScoped<IBaseRepository<UserCollectionType>, BaseRepository<UserCollectionType>>();
+            services.AddScoped<IBaseRepository<UserCollection>, BaseRepository<UserCollection>>();
+            services.AddScoped<IBaseRepository<UserType>, BaseRepository<UserType>>();
 
             services.AddScoped<ISmtpClient, SmtpClient>();
             services.AddScoped<IEmailService, EmailService>();
