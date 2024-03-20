61be2bd9d044d67aa9f39a2f6eda1583584a56f3
Błażej Zajac <blazezaj@gmail.com>
2024-03-20 15:17:51 +0100

    Create VideoLinkHelper.cs
    Add VideoLinkHelper responsible for automatic conversion of user-entered video links to the correct format, eliminating the need for manual formatting.

---
+++ NextVer.Api/Helpers/VideoLinkHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NextVerBackend.Helpers
+{
+    public class VideoLinkHelper
+    {
+        public static string ConvertVideoLink(string inputLink)
+        {
+            var regex = new Regex(@"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})");
+            var match = regex.Match(inputLink);
+
+            if (match.Success)
+            {
+                var videoId = match.Groups[1].Value;
+                return $"https://www.youtube.com/embed/{videoId}";
+            }
+
+            return inputLink;
+        }
+    }
+}
