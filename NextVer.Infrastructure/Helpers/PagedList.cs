328e01674a46f4cc1f3b29c656cd7caac02846a0
Błażej Zajac <blazezaj@gmail.com>
2024-03-20 14:38:42 +0100

    Create PagedList.cs

---
+++ NextVer.Infrastructure/Helpers/PagedList.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NextVer.Infrastructure.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(List<T> items, int currentPage, int totalCount, int pageSize)
+        {
+            CurrentPage = currentPage;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            PageSize = pageSize;
+            TotalCount = totalCount;
+            AddRange(items);
+        }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var totalCount = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, pageNumber, totalCount, pageSize);
+        }
+    }
+}
